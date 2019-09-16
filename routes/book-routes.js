4c352d6e23abacdbc6aa5a365b830a221d2e9c46
alejandrogomez <alegomez1@me.com>
2019-09-16 12:29:05 -0400

    Create new book form added and redirects

--- routes/book-routes.js
+++ routes/book-routes.js
@@ -15,16 +15,10 @@ router.get('/books', (req, res, next)=>{
     next(err)
   })
 
-
-
-
-
-
-
 })
 
 
-router.get('/books/:theid', (req, res, next)=>{
+router.get('/books/details/:theid', (req, res, next)=>{
 
 
 let id = req.params.theid
@@ -38,10 +32,41 @@ Book.findById(id)
   next(err)
 })
 
+})
 
+
+
+router.get('/books/create-new-book', (req, res, next)=>{
+
+
+
+  res.render('book-views/new-book')
 
 
 })
+
+
+router.post('/books/creation', (req,res,next)=>{
+
+let title = req.body.theTitle;
+let author = req.body.theAuthor;
+let image = req.body.theImage;
+
+Book.create({
+  title: title,
+  author: author,
+  image: image
+})
+.then((result)=>{
+
+  res.redirect('/books')
+
+})
+.catch((err)=>
+next(err))
+})
+
+
 
 
 
